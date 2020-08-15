0e1663d86f18cc290a136605ef98c54f0785464d
Danilo Alves <danilovieiraalves@hotmail.com>
2020-08-15 00:38:30 -0300

    feat: Uses global style in index

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import ReactDOM from "react-dom";
 // App
 import App from "./App";
 
+// Styles utilities
+import { GlobalStyle } from "./styles";
+
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
+
     <App />
   </React.StrictMode>,
   document.getElementById("root")
